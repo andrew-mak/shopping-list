8c7057c3733b5b0d3c06bf4e413b9f0124c7e857
Andrei <andrews.makarevich@gmail.com>
2021-02-06 01:23:05 +0300

    dev: implemented remove item, filter on input
    * remove item on server
    * filtering items by typing
    * cleanup function in useEffect
    * used useCallback, useRef

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -6,21 +6,10 @@ import Search from './Search';
 
 function Ingredients() {
   const [ingredients, setIngredientsState] = useState([]);
-  useEffect(() => {
-    fetch('https://react-hooks-ea382-default-rtdb.firebaseio.com/ingredients.json')
-      .then(response => response.json())
-      .then(responseData => {
-        const loadedIngredients = [];
-        for (const key in responseData) {
-          loadedIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount,
-          })
-        }
-        setIngredientsState(loadedIngredients)
-      });
-  }, []);
+
+  const filtredIngredientsHandler = useCallback(filtredIngredients => {
+    setIngredientsState(filtredIngredients);
+  }, [setIngredientsState]);
 
   const addIngredientHandler = ingredient => {
     fetch('https://react-hooks-ea382-default-rtdb.firebaseio.com/ingredients.json', {
@@ -36,7 +25,11 @@ function Ingredients() {
   };
 
   const removeIngredientHandler = itemId => {
-    setIngredientsState(prevIngredients => prevIngredients.filter(ingredient => ingredient.id !== itemId));
+    fetch(`https://react-hooks-ea382-default-rtdb.firebaseio.com/ingredients/${itemId}.json`, {
+      method: 'DELETE'
+    }).then(response => {
+      setIngredientsState(prevIngredients => prevIngredients.filter(ingredient => ingredient.id !== itemId));
+    });
   }
 
   return (
@@ -44,7 +37,7 @@ function Ingredients() {
       <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
-        <Search />
+        <Search onLoadIngredients={filtredIngredientsHandler} />
         <IngredientList ingredients={ingredients}
           onRemoveItem={removeIngredientHandler}
         />
