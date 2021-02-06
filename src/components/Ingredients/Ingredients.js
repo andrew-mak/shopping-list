1e73cc0453ac44d983c4c4a2495de711ea896d3d
Andrei <andrews.makarevich@gmail.com>
2021-02-06 14:57:00 +0300

    dev: add spinner, manage multiple states
    implemented  Loading indicator component
    State managing: used multiple state and managed separately

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -2,21 +2,26 @@ import React, { useState, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
+import ErrorModal from '../UI/ErrorModal';
 import Search from './Search';
 
 function Ingredients() {
   const [ingredients, setIngredientsState] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
   const filtredIngredientsHandler = useCallback(filtredIngredients => {
     setIngredientsState(filtredIngredients);
   }, [setIngredientsState]);
 
   const addIngredientHandler = ingredient => {
+    setIsLoading(true);
     fetch('https://react-hooks-ea382-default-rtdb.firebaseio.com/ingredients.json', {
       method: 'POST',
       body: JSON.stringify(ingredient),
       headers: { 'Content-Type': 'application/json' }
     }).then(response => response.json()).then(responseData => {
+      setIsLoading(false);
       setIngredientsState(prevIngredients => [
         ...prevIngredients,
         { id: responseData.name, ...ingredient }
@@ -25,16 +30,29 @@ function Ingredients() {
   };
 
   const removeIngredientHandler = itemId => {
+    setIsLoading(true);
     fetch(`https://react-hooks-ea382-default-rtdb.firebaseio.com/ingredients/${itemId}.json`, {
       method: 'DELETE'
     }).then(response => {
+      setIsLoading(false);
       setIngredientsState(prevIngredients => prevIngredients.filter(ingredient => ingredient.id !== itemId));
+    }).catch(error => {
+      setIsLoading(false);
+      setError('Something went wrong :(');
     });
+  };
+
+  const clearError = () => {
+    setError(null);
   }
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler} />
+      {error && <ErrorModal onClose={clearError} >{error}</ErrorModal>}
+      <IngredientForm
+        onAddIngredient={addIngredientHandler}
+        loading={isLoading}
+      />
 
       <section>
         <Search onLoadIngredients={filtredIngredientsHandler} />
