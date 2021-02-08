a2406c32dd649540e0933e90c729ca84e9852126
Andrei <andrews.makarevich@gmail.com>
2021-02-08 22:21:42 +0300

    dev: added Auth imitation, AuthContext
    implemented:
    * AuthForm for authentication  imitation
    * AuthContextProvider to provide Auth status
    Hooks: useContext

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ReactContextProvider from './context/auth-context';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ReactContextProvider>
+      <App />
+    </ReactContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
